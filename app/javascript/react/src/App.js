335cf677c52a1499f34c177c7764dadb264d55dc
devlin-person <devylin.91@gmail.com>
2017-10-31 13:08:57 -0400

    Created and set up icons

--- app/javascript/react/src/App.js
+++ app/javascript/react/src/App.js
@@ -7,8 +7,14 @@ const App = props => {
       <div className="photoScroll">
         <h4>photos</h4>
       </div>
-      <div className="icon list">
-        <h4>icons</h4>
+      <div className="icon-list">
+        <i class="flaticon-black icon cat"></i>
+        <i class="flaticon-animals icon dog"></i>
+        <i class="flaticon-dove icon bird"></i>
+        <i class="flaticon-users-group icon people"></i>
+        <i class="flaticon-drink icon food"></i>
+        <i class="flaticon-mountain icon scenery"></i>
+        <i class="flaticon-city icon buliding"></i>
       </div>
     </div>
   )
