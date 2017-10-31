85608c02a65ed6cf14b8bb3f8705184eb6e526f1
devlin-person <devylin.91@gmail.com>
2017-10-31 11:25:24 -0400

    Updated home page test

--- app/javascript/react/src/App.js
+++ app/javascript/react/src/App.js
@@ -2,7 +2,15 @@ import React from 'react';
 
 const App = props => {
   return(
-    <h1>Hello World</h1>
+    <div>
+      <h1>Textless Site</h1>
+      <div className="photoScroll">
+        <h4>photos</h4>
+      </div>
+      <div className="icon list">
+        <h4>icons</h4>
+      </div>
+    </div>
   )
 }
 
