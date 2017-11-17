b03a05449b23cd1d756cbb93c78d203f8a973c8f
devlin-person <devylin.91@gmail.com>
2017-11-17 18:02:23 -0500

    Dried up links

--- app/javascript/react/src/containers/IconsContainer.js
+++ app/javascript/react/src/containers/IconsContainer.js
@@ -22,38 +22,38 @@ class IconsContainer extends React.Component {
         <IconComponent
           class="flaticon-black icon cat"
           // displayPicture={cat_display}
-          pic_link="/cat_photo"
+          // pic_link="/cat_photo"
+          test="test"
         />
         <IconComponent
           class="flaticon-animals icon dog"
           // displayPicture={dog_display}
-          pic_link="/dog_photo"
+          // pic_link="/dog_photo"
         />
         <IconComponent
           class="flaticon-dove icon bird"
           // displayPicture={bird_display}
-          pic_link="/bird_photo"
+          // pic_link="/bird_photo"
         />
         <IconComponent
           class="flaticon-users-group icon people"
           // displayPicture={people_display}
-          pic_link="/people_photo"
+          // pic_link="/people_photo"
         />
         <IconComponent
           class="flaticon-drink icon food"
           // displayPicture={food_display}
-          pic_link="/food_photo"
-          test="this"
+          // pic_link="/food_photo"
         />
         <IconComponent
           class="flaticon-mountain icon scenery"
           // displayPicture={scenery_display}
-          pic_link="/scenery_photo"
+          // pic_link="/scenery_photo"
         />
         <IconComponent
           class="flaticon-city icon buliding"
           // displayPicture={building_display}
-          pic_link="/building_photo"
+          // pic_link="/building_photo"
         />
       </div>
     )
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   resources :pictures
   resources :photo_display
-  resources :bird_photo
-  resources :cat_photo
-  resources :dog_photo
-  resources :food_photo
-  resources :people_photo
-  resources :building_photo
-  resources :scenery_photo
+  # resources :bird_photo
+  # resources :cat_photo
+  # resources :dog_photo
+  # resources :food_photo
+  # resources :people_photo
+  # resources :building_photo
+  # resources :scenery_photo
 
   root 'static_pages#index'
 end
